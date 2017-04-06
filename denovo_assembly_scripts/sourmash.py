da83856a689834cab8f9c581fb809b1e9ffa9367
Lisa Cohen <ljcohen@ucdavis.edu>
2017-04-06 10:16:01 -0700

    updated sourmash.py

--- denovo_assembly_scripts/sourmash.py
+++ denovo_assembly_scripts/sourmash.py
@@ -3,8 +3,11 @@ import os.path
 import clusterfunc_py3
 
 def run_trim_low_abund(trim_filename,trim_low_abund_dir):
+#    trim_low_abund="""
+#trim-low-abund.py -k 21 -C 2 --variable-coverage {}
+#""".format(trim_filename)
     trim_low_abund="""
-trim-low-abund.py -k 21 -C 2 --variable-coverage {}
+trim-low-abund.py -k 21 -C 2 {}
 """.format(trim_filename)
     commands = [trim_low_abund]
     process_name = "trim-low-abund"
@@ -36,10 +39,22 @@ sourmash compute --force -k 21 --scaled 20000 {}
     print(sourmash_command)
     clusterfunc_py3.sbatch_file(sourmash_dir,process_name, module_name_list, filename, commands)
 
+def get_sourmash_streaming(sourmash_filename,sourmash_dir,genus_species_sample):
+    sourmash_command="""
+head  -n 4000000 {} | sourmash compute -k 51 --name {} --scaled 500 --dna - -o {}{}.sig
+""".format(sourmash_filename,genus_species_sample[:-10],sourmash_dir,genus_species_sample)
+    commands = [sourmash_command]
+    process_name = "sourmash"
+    module_name_list = ""
+    filename = genus_species_sample
+    print(sourmash_command)
+    clusterfunc_py3.sbatch_file(sourmash_dir,process_name, module_name_list, filename, commands)
+
 #trim_dir = "/home/ljcohen/osmotic_trim/"
 trim_low_abund_dir = "/home/ljcohen/osmotic_trim/trim_low_abund/"
 #trim_dir = "/home/ljcohen/mislabeled_files_removed_osmotic/"
-trim_dir = "/home/ljcohen/osmotic_trim/trim_low_abund/sbatch_files/"
+#trim_dir = "/home/ljcohen/osmotic_trim/trim_low_abund/sbatch_files/"
+trim_dir = "/home/ljcohen/osmotic_trim/trim_low_abund/"
 #trim_dir = "/home/ljcohen/osmotic_sourmash/"
 sourmash_dir = "/home/ljcohen/osmotic_sourmash/"
 trim_files = os.listdir(trim_dir)
@@ -50,11 +65,12 @@ for filename in trim_files:
     #if filename.endswith(".head"):
     if filename.endswith(".abundtrim"):
         filename_info = filename.split("_")
-        genus_species_sample = filename_info[0] + "_" + filename_info[1] + "_" + filename_info[2] + "_" + filename_info[3][:1]
-        #trim_filename = trim_dir + filename
+        genus_species_sample = filename_info[0] + "_" + filename_info[1] + "_" + filename_info[2] + "_" + filename_info[3][:1] + "_" + filename_info[4]
+        trim_filename = trim_dir + filename
         sourmash_filename = trim_dir  + filename
         #head_reads(sourmash_filename,sourmash_dir,filename)
-        sourmash_files = [s for s in complete_sourmash_files if s.startswith(genus_species_sample)]
+        #sourmash_files = [s for s in complete_sourmash_files if s.startswith(genus_species_sample)]
         #run_trim_low_abund(trim_filename,trim_low_abund_dir)
-        if len(sourmash_files) <1:
-            get_sourmash_command(sourmash_filename,sourmash_dir,genus_species_sample)
+        #if len(sourmash_files) <1:
+            #get_sourmash_command(sourmash_filename,sourmash_dir,genus_species_sample)
+        get_sourmash_streaming(sourmash_filename,sourmash_dir,genus_species_sample)
