36854cd4937d2dca9fe326dd3d434fd63320896d
Lisa Cohen <ljcohen@ucdavis.edu>
2018-05-23 14:22:28 -0400

    msu hpcc changes

--- denovo_assembly_scripts/busco.py
+++ denovo_assembly_scripts/busco.py
@@ -6,14 +6,18 @@ import clusterfunc_py3
 
 def run_busco(fasta,sample,buscodir):
     busco_command="""
-run_BUSCO.py -i {} -o {} -l ~/bin/busco/metazoa_odb9 -m tran --cpu 8
+module unload python
+source /home/ljcohen/.bashrc
+conda activate dammit_new
+cd /mnt/scratch/ljcohen/osmotic_killifish/busco
+run_BUSCO.py -i {} -o {}.actino -l ~/reference/actinopterygii_odb9 -m tran --cpu 16
 """.format(fasta,sample)
     print(busco_command)
     commands = [busco_command]
     process_name = "busco"
     module_name_list = ""
     filename = sample
-    clusterfunc_py3.sbatch_file(buscodir, process_name,module_name_list, filename, commands)
+    clusterfunc_py3.qsub_file(buscodir, process_name,module_name_list, filename, commands)
 
 def execute(assemblies,assemblydir,buscodir):
     for assembly in assemblies:
@@ -22,7 +26,7 @@ def execute(assemblies,assemblydir,buscodir):
             sample = assembly.split(".")[0]
             run_busco(fasta,sample,buscodir)
  
-buscodir = "/home/ljcohen/osmotic_busco_v3/metazoa/"
-assemblydir = "/home/ljcohen/public_html/killifish/transcriptome_assemblies/"
+buscodir = "/mnt/scratch/ljcohen/osmotic_killifish/busco/"
+assemblydir = "/home/ljcohen/osmotic_killifish_assemblies/assemblies/"
 listoffiles = os.listdir(assemblydir)
 execute(listoffiles,assemblydir,buscodir)
--- denovo_assembly_scripts/clusterfunc_py3.py
+++ denovo_assembly_scripts/clusterfunc_py3.py
@@ -6,50 +6,60 @@ import os
 import subprocess
 from subprocess import Popen, PIPE
 
-def check_dir(dirname):
-    if os.path.isdir(dirname)==False:
-        os.mkdir(dirname)
-        print("Directory created:",dirname)
 
-def get_sbatch_filename(basedir,process_name,filename):
-    sbatch_dir=basedir+"sbatch_files/"
-    #sbatch_dir="/home/ljcohen/bin/"
-    check_dir(sbatch_dir)
-    sbatch_filename=sbatch_dir+process_name+"_"+filename+".sbatch"
-    return sbatch_dir,sbatch_filename
+def check_dir(dirname):
+    if os.path.isdir(dirname) == False:
+        os.mkdir(dirname)
+        print("Directory created:", dirname)
+
+
+def get_qsub_filename(basedir, process_name, filename):
+    qsub_dir = basedir + "qsub_files/"
+    check_dir(qsub_dir)
+    qsub_filename = qsub_dir + process_name + "_" + filename + ".qsub"
+    return qsub_dir, qsub_filename
+
 
 def get_module_load_list(module_name_list):
-    module_list=[]
+    module_list = []
     for module in module_name_list:
-        module_load="module load "+module
+        module_load = "module load " + module
         module_list.append(module_load)
     return module_list
 
-def sbatch_file(basedir,process_name,module_name_list,filename,process_string):
-    working_dir=os.getcwd()
-    sbatch_dir,sbatch_filename=get_sbatch_filename(basedir,process_name,filename)
-    os.chdir(sbatch_dir)
-    module_load=get_module_load_list(module_name_list)
-    with open(sbatch_filename,"w") as sbatch_file:
-        sbatch_file.write("#!/bin/bash -l"+"\n")
-        sbatch_file.write("#SBATCH -D "+sbatch_dir+"\n")
-        sbatch_file.write("#SBATCH -J "+process_name+"\n")
-        #sbatch_file.write("#SBATCH -p bigmemh"+"\n")
-        #sbatch_file.write("#SBATCH -A millermrgrp"+"\n")
-        sbatch_file.write("#SBATCH -t 24:00:00"+"\n")
-        sbatch_file.write("#SBATCH -N 1"+"\n")
-        sbatch_file.write("#SBATCH -n 1"+"\n")
-        sbatch_file.write("#BATCH -p high\n")
-        sbatch_file.write("#SBATCH -c 8"+"\n")
-        sbatch_file.write("#SBATCH --mem=48000"+"\n")
-        for module_string in module_load:
-            sbatch_file.write(module_string+"\n")
-            #print(module_string)
+
+def qsub_file(basedir, process_name, module_name_list, filename, process_string):
+    working_dir = os.getcwd()
+    qsub_dir, qsub_filename = get_qsub_filename(
+        basedir, process_name, filename)
+    os.chdir(qsub_dir)
+    module_list = get_module_load_list(module_name_list)
+# directive to use Laconia
+# #PBS -l feature=intel16
+#export MKL_NUM_THREADS=8
+#export OMP_NUM_THREADS=8
+# for ged lab nodes only
+#PBS -A ged
+    f = """#!/bin/bash
+#PBS -l walltime=24:00:00,nodes=1:ppn=24
+#PBS -l mem=200gb
+#PBS -A ged
+#PBS -j oe
+cd ${{PBS_O_WORKDIR}}
+""".format()
+    with open(qsub_filename, "w") as qsub:
+        qsub.write(f)
+        for module_string in module_list:
+            qsub.write(module_string + "\n")
         for string in process_string:
-            sbatch_file.write(string+"\n")
-            print(string)
-    sbatch_string="sbatch --get-user-env "+sbatch_filename
-    print(sbatch_string)
-    #s=subprocess.Popen(sbatch_string,shell=True)
-    #s.wait()
+            qsub.write(string + "\n")
+        qsub.write("qstat -f ${PBS_JOBID}\n")
+        qsub.write(
+            "cat ${PBS_NODEFILE} # Output Contents of the PBS NODEFILE\n")
+        qsub.write(
+            "env | grep PBS # Print out values of the current jobs PBS environment variables\n")
+    qsub_string = 'qsub -V ' + qsub_filename
+    print(qsub_string)
+    s = subprocess.Popen(qsub_string, shell=True)
+    s.wait()
     os.chdir(working_dir)
