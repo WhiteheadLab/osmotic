ec5666b78b8c0733a9fc2d8ee279588571a1120d
Lisa Johnson <ljcohen@br018.pvt.bridges.psc.edu>
2018-12-07 01:30:03 -0500

    old v new transrate

--- denovo_assembly_scripts/bridges/transrate_ref_bridges.py
+++ denovo_assembly_scripts/bridges/transrate_ref_bridges.py
@@ -49,15 +49,15 @@ def execute(data_frame1,data_frame2,new_assemblies,old_assemblies,new_assemblies
             print(fasta)
             sample = fasta.split(".")[0]
             trinity_fasta = new_assemblies_dir + fasta
-            #old_fasta = [s for s in old_assemblies if s.split(".")[0] == sample] 
-            #if len(old_fasta) > 0:
-            #    reference = old_assemblies_dir + old_fasta[0]
-            #else:
-            #    print(old_fasta)
-            transrate_out_forward = transrate_dir + sample + "_trinity_v_Fhet.NCBIrna/"
-            transrate_out_reverse = transrate_dir + sample + "_Fhet.NCBIrna_v_trinity/"
-            #transrate_out_forward = transrate_dir + sample + "_trinity_v_old/"
-            #transrate_out_reverse = transrate_dir + sample + "_old_v_trinity/"
+            old_fasta = [s for s in old_assemblies if s.split(".")[0] == sample] 
+            if len(old_fasta) > 0:
+                reference = old_assemblies_dir + old_fasta[0]
+            else:
+                print(old_fasta)
+            #transrate_out_forward = transrate_dir + sample + "_trinity_v_Fhet.NCBIrna/"
+            #transrate_out_reverse = transrate_dir + sample + "_Fhet.NCBIrna_v_trinity/"
+            transrate_out_forward = transrate_dir + sample + "_trinity_v_old/"
+            transrate_out_reverse = transrate_dir + sample + "_old_v_trinity/"
             transrate_assemblies_forward = transrate_out_forward + "assemblies.csv"
             transrate_assemblies_reverse = transrate_out_reverse + "assemblies.csv"
             if os.path.isfile(transrate_assemblies_forward):
@@ -80,8 +80,8 @@ def execute(data_frame1,data_frame2,new_assemblies,old_assemblies,new_assemblies
 reference = "/pylon5/bi5fpmp/ljcohen/Fhet_reference_genome/ncbi/rna.fa"
 #reference = "/pylon5/bi5fpmp/ljcohen/Fhet_reference_genome/ensembl/Fundulus_heteroclitus.Fundulus_heteroclitus-3.0.2.cds.all.fa"
 #reference = "/pylon5/bi5fpmp/ljcohen/Fhet_reference_genome/ensembl/Fundulus_heteroclitus.Fundulus_heteroclitus-3.0.2.cdna.all.fa"
-#transrate_dir = "/pylon5/bi5fpmp/ljcohen/kfish_transrate/"
-transrate_dir = "/pylon5/bi5fpmp/ljcohen/kfish_transrate_NCBI/"
+transrate_dir = "/pylon5/bi5fpmp/ljcohen/kfish_transrate/"
+#transrate_dir = "/pylon5/bi5fpmp/ljcohen/kfish_transrate_NCBI/"
 old_assemblies_dir = "/pylon5/bi5fpmp/ljcohen/kfish_assemblies_old/"
 new_assemblies_dir = "/pylon5/bi5fpmp/ljcohen/kfish_trinity/"
 old_assemblies = os.listdir(old_assemblies_dir)
@@ -89,7 +89,7 @@ new_assemblies = os.listdir(new_assemblies_dir)
 data_frame1 = pd.DataFrame()
 data_frame2 = pd.DataFrame()
 data_frame1,data_frame2 = execute(data_frame1,data_frame2,new_assemblies,old_assemblies,new_assemblies_dir,old_assemblies_dir,transrate_dir,reference)
-data_frame1.to_csv("../../evaluation_data/transrate_reference_trinity_v_Fhet.NCBI_Dec2018.csv")
-data_frame2.to_csv("../../evaluation_data/transrate_reference_Fhet.NCBI_v_trinity_Dec2018.csv")
-print("Forward transrate reference written: ../../evaluation_data/transrate_reference_trinity_v_Fhet.NCBI.csv")
-print("Reverse transrate reference written: ../../evaluation_data/transrate_reference_Fhet.NCBI_v_trinity.csv")
+data_frame1.to_csv("../../evaluation_data/transrate_reference_trinityDec2018_v_old.csv")
+data_frame2.to_csv("../../evaluation_data/transrate_reference_old_v_trinityDec2018.csv")
+print("Forward transrate reference written: ../../evaluation_data/transrate_reference_trinityDec2018_v_old.csv")
+print("Reverse transrate reference written: ../../evaluation_data/transrate_reference_old_v_trinityDec2018.csv")
